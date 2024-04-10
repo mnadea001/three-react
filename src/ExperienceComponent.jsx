6fa596238a400d62f7248a624f2f8ae111ef5bc2
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-10 14:57:04 +0200

    orbitControls

--- src/ExperienceComponent.jsx
+++ src/ExperienceComponent.jsx
@@ -1,16 +1,26 @@
-import { useFrame } from "@react-three/fiber"
+
 import { useRef } from "react"
+import { useThree, extend, useFrame } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+extend({ OrbitControls })
+
 export default function ExperienceComponent() {
+
+    const {camera, gl} = useThree()
 
     const cubeRef = useRef()
     const groupRef = useRef()
 
-
     useFrame((state, delta)=> {
-cubeRef.current.rotation.y += delta   
-groupRef.current.rotation.y += delta  
+
+        cubeRef.current.rotation.y += delta   
+
 })
+
 return <>
+<orbitControls args={[ camera, gl.domElement]}/>
+
 <group ref={groupRef}>
 
   <mesh position-x={-2} rotation-x={Math.PI * 0.23}>
