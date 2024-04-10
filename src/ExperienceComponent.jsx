494d6315f1af25dcd6b3f15ae9bff8137c9476c3
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-10 15:01:33 +0200

    lights

--- src/ExperienceComponent.jsx
+++ src/ExperienceComponent.jsx
@@ -21,21 +21,24 @@ export default function ExperienceComponent() {
 return <>
 <orbitControls args={[ camera, gl.domElement]}/>
 
+<directionalLight position={[ 1, 2, 3]} intensity={ 4.5}/>
+<ambientLight intensity={ 1.5}/>
+
 <group ref={groupRef}>
 
   <mesh position-x={-2} rotation-x={Math.PI * 0.23}>
     <sphereGeometry scale={1.5}/>
-    <meshBasicMaterial color="orange" />
+    <meshStandardMaterial color="orange" />
   </mesh>
 
   <mesh ref={ cubeRef } scale={1.5} position-x={2} rotation-y={Math.PI * 0.25}>
     <boxGeometry scale={1.5}/>
-    <meshBasicMaterial color="mediumpurple" />
+    <meshStandardMaterial color="mediumpurple" />
   </mesh>
 
   <mesh scale={10} position-y={-1} rotation-x={- Math.PI * 0.5}>
     <planeGeometry/>
-    <meshBasicMaterial color="greenyellow" />
+    <meshStandardMaterial color="greenyellow" />
   </mesh>
 
 </group>
